1e6ea0a2b01664b7d7b20cc323bd230e7097cf2e
sanjeevaReddych <sanjevareddy.ch@gmail.com>
2022-02-15 08:49:33 +0530

    Update analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -1,6 +1,6 @@
 SONAR_SCANNER_VERSION=4.6.1.2450 # Find the latest version in the "Linux" link on this page:
                                  # https://docs.sonarqube.org/latest/analysis/scan/sonarscanner/
-SONAR_SERVER_URL="http://localhost:9000/" # To fill in
+SONAR_SERVER_URL="http://localhost:9000" # To fill in
 BW_OUTPUT=bw-output # same directory than the one set in `sonar-project.properties`
 
 curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}-linux.zip"
